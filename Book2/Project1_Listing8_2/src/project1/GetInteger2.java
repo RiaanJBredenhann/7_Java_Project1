a831513b048113275837e432456fea764b9a3927
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-11 17:12:30 +0200

    update

---
+++ Book2/Project1_Listing8_2/src/project1/GetInteger2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1;
+
+import java.util.*;
+
+public class GetInteger2 {
+
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        System.out.print("Enter an integer: ");
+        int i = GetAnInteger();
+        System.out.println("You entered " + i);
+    }
+    
+    public static int GetAnInteger()
+    {
+        while (!sc.hasNextInt())
+        {
+            sc.nextLine();
+            System.out.print("That's not an integer. Try again: ");
+        }
+    return sc.nextInt();
+    }
+    
+}
---
+++ Book2/Project1_Listing8_3/src/project1/CrazyWithZeroes.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1;
+
+public class CrazyWithZeroes {
+
+    public static void main(String[] args) {
+        try
+        {
+            int answer = divideTheseNumbers(5, 0);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Tried twice, still didn't work!");
+        }
+    }
+    
+    public static int divideTheseNumbers(int a, int b) throws Exception
+    {
+        int c;
+        
+        try
+        {
+            c = a / b;
+            System.out.println("It worked!");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Didn't work the first time.");
+            c = a / b;
+            System.out.println("It worked the second time!");
+        }
+        finally
+        {
+            System.out.println("Better clean up my mess."); 
+        }
+        System.out.println("It worked after all."); 
+        return c; 
+    }   
+}
