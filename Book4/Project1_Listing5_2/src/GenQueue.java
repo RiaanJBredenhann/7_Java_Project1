d44119032e80624ee490772694258fc72c2398a8
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-15 16:18:12 +0200

    update

---
+++ Book4/Project1_Listing5_2/src/GenQueue.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.*;
+
+public class GenQueue<E>
+{
+    private LinkedList<E> list = new LinkedList<E>(); 
+    
+    public void enqueue(E item) 
+    {
+        list.addLast(item);
+    }
+    
+    public E dequeue() 
+    {
+        return list.poll();
+    }
+    
+    public boolean hasItems() 
+    {
+        return !list.isEmpty();
+    }
+    
+    public int size() 
+    {
+        return list.size();
+    }
+    
+    public void addItems(GenQueue<? extends E> q) 
+    {
+        while (q.hasItems())
+        list.addLast(q.dequeue());
+    }
+}
