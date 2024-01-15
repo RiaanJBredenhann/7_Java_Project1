7c4902e4b0f995f16bdef809f87411100fbb67f1
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-15 16:10:23 +0200

    update

---
+++ Book4/Project1_Listing2_3/src/project1/CopyDemo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1;
+
+import java.util.Arrays;
+
+public class CopyDemo {
+
+    public static void main(String[] args) {
+        int arrayOriginal[] = {42, 55, 21};
+        int arrayNew[] = Arrays.copyOf(arrayOriginal, 3);
+        printIntArray(arrayNew);
+    }
+    
+    static void printIntArray(int arrayNew[])
+    {
+        for (int i : arrayNew)
+        {
+            System.out.print(i);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+    
+}
